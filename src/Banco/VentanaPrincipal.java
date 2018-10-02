563ab83d90698e19af44c62d02398a33c4ed87bd
joaco1916 <joaquin_a_rodriguez@hotmail.com>
2018-10-02 16:33:25 -0300

    Corrección del primer commit.

--- src/Banco/VentanaPrincipal.java
+++ src/Banco/VentanaPrincipal.java
@@ -50,6 +50,8 @@ public class VentanaPrincipal extends JFrame {
 		JMenu mFunciones = new JMenu("Funciones");
 		menuBar.add(mFunciones);
 		
+		getContentPane().setLayout(null);
+		
 		JDesktopPane desktopPane = new JDesktopPane();
 		desktopPane.setBounds(0, 0, 1, 1);
 		getContentPane().add(desktopPane);
